d7f556cc160c41299fa8706ba88e43e7325b24bd
AjithPanneerselvam <ajithpanneerselvam20@gmail.com>
2020-12-13 14:57:30 +0530

    Logging and env setup

---
+++ src/config.rs
@@ -0,0 +1,19 @@
+use envy;
+
+#[derive(Deserialize, Debug)]
+pub struct Config {
+    pub proxy_listen_port: String,
+
+    pub redis_address: String,
+
+    pub is_tls_on: bool,
+
+    pub fault_api_listen_port: String,
+}
+
+pub fn get_config() -> Result<Config, envy::Error> {
+    match envy::from_env::<Config>() {
+        Ok(config) => return Ok(config),
+        Err(e) => return Err(e),
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use env_logger::Env;
+use log::debug;
+
+#[macro_use]
+extern crate serde_derive;
+
+mod config;
+
+fn init_logger() {
+    let env = Env::default()
+        .filter_or("LOG_LEVEL", "debug")
+        .write_style_or("LOG_STYLE", "always");
+
+    env_logger::init_from_env(env);
+}
+
 fn main() {
-    println!("Hello, world!");
+    init_logger();
+
+    let config = config::get_config().unwrap();
+    debug!("env configs: {:?}", config);
 }
